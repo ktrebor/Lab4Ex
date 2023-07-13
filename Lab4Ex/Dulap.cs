11629b339b2fc97ecf731996194520141f1a1188
Robert <robert.kedves.90@gmail.com>
2023-07-13 17:55:01 +0300

    Add project files.

---
+++ Lab4Ex/Dulap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Ex1
+{
+    internal class Dulap
+    {
+        private readonly double lungime;
+        private readonly double latime;
+        private readonly double inaltime;
+        
+        public Dulap(double lungime, double latime, double inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+
+        public double GetVolum()
+        {
+            return lungime * latime * inaltime;
+        }
+    }
+}
---
+++ Lab4Ex/Program.cs
@@ -0,0 +1,23 @@
+﻿using Lab4Ex1;
+using System;
+
+namespace Lab4Ex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdu masurile unui dulap");
+            Console.Write("Lungime: ");
+            double lungime = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Latime: ");
+            double latime = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Inaltime: ");
+            double inaltime = Convert.ToDouble(Console.ReadLine());
+
+            Dulap bucatarie = new Dulap(lungime, latime, inaltime);
+            
+            Console.WriteLine($"Volumul este {bucatarie.GetVolum()}");
+        }
+    }
+}
