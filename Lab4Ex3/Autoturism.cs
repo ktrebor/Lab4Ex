7ea973e3424d146068ac3473a7ab000196a19079
Robert <robert.kedves.90@gmail.com>
2023-07-16 16:05:43 +0300

    ex3

---
+++ Lab4Ex3/Autoturism.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Ex3
+{
+    class Autoturism
+    {
+        private string marca = string.Empty;
+        private string numarInmatriculare = string.Empty;
+        private readonly int capacitateCilindrica;
+        private List<string> listaPasageri = new List<string>();
+
+        public Autoturism(string marca, string numarInmatriculare, int capacitateCilindrica)
+        {
+            this.marca = marca;
+            this.numarInmatriculare = numarInmatriculare;
+            this.capacitateCilindrica = capacitateCilindrica;
+        }
+
+        public string GetDescription()
+        {
+            string mesajPasager = string.Empty;
+
+            if (listaPasageri.Count > 0)
+            {
+                foreach (var pasager in listaPasageri)
+                {
+                    mesajPasager = string.Join(", ", listaPasageri);
+                }
+            } 
+            else
+            {
+                mesajPasager = "Fara pasageri ";
+            }
+
+            string descriere = $"Marca: {marca}, Numar de inmatriculare: {numarInmatriculare}, capacitate cilindrica: {capacitateCilindrica}. Pasageri: {mesajPasager}, ";
+
+            return descriere;
+        }
+
+        public void AdaugaPasager(string pasager)
+        {
+            listaPasageri.Add(pasager);
+        }
+
+        public void StergePasager(string pasager)
+        {
+            listaPasageri.Remove(pasager);
+        }
+
+        public bool EstePasager(string pasager)
+        {
+            return listaPasageri.Contains(pasager);
+        }
+    }
+}
--- Lab4Ex3/Program.cs
+++ Lab4Ex3/Program.cs
@@ -4,9 +4,23 @@ namespace Lab4Ex3
 {
     internal class Program
     {
+        /*Autoturismului ii putem adauga pasageri sub forma unor siruri de caractere*/
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {     
+            Autoturism vehiculFaraLimita = new Autoturism("VW", "SB12CCC", 2000);
+            vehiculFaraLimita.AdaugaPasager("Ion");
+            vehiculFaraLimita.AdaugaPasager("Costel");
+            vehiculFaraLimita.AdaugaPasager("Irina");
+
+            Console.WriteLine(vehiculFaraLimita.GetDescription());
+
+            Console.WriteLine(vehiculFaraLimita.EstePasager("Ion"));
+
+            vehiculFaraLimita.StergePasager("Ion");
+
+            Console.WriteLine(vehiculFaraLimita.EstePasager("Ion"));
+
+            Console.WriteLine(vehiculFaraLimita.GetDescription());
         }
     }
 }
